7b6d3d89cb0054a7e78d3f826c6eb1465f5b4f16
44173465NHA <nguyenvietbn1999@gmail.com>
2020-06-01 23:44:40 +0700

    Delete All Trash

--- src/GamePlayer.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Fresher_LOCAL on 6/1/2020.
- */
-var GamePlayer = cc.Sprite.extend({
-
-});
--- src/SysMenu.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Admin on 6/1/2020.
- */
-var SysMenu = cc.Layer.extend({
-
-});
