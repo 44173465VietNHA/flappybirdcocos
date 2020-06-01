12a20e1e09aacafa16867b87e2405d58747a184b
44173465NHA <nguyenvietbn1999@gmail.com>
2020-06-01 23:20:14 +0700

    change struct

--- main.js
+++ main.js
@@ -1,39 +1,22 @@
-
-var gv = gv || {};
-
-var DESIGN_RESOLUTION_WIDTH = 1136;
-var DESIGN_RESOLUTION_HEIGHT = 640;
-cc.game.onStart = function () {
-    if (!cc.sys.isNative && document.getElementById("cocosLoading")) //If referenced loading.js, please remove it
-        document.body.removeChild(document.getElementById("cocosLoading"));
-    // Pass true to enable retina display, disabled by default to improve performance
-    cc.view.enableRetina(true);
-    // Adjust viewport meta
+cc.game.onStart = function(){
+    cc.view.enableRetina(false);
     cc.view.adjustViewPort(true);
-    jsb.fileUtils.addSearchPath(fr.NativeService.getFolderUpdateAssets(), true);
-    jsb.fileUtils.addSearchPath(fr.NativeService.getFolderUpdateAssets() + "/res", true);
-    cc.loader.resPath = "res";
-    cc.LoaderScene.preload(g_resources, function () {
-        //hide fps
-        cc.director.setDisplayStats(true);
-        // Setup the resolution policy and design resolution size
-        var frameSize = cc.view.getFrameSize();
-        var ratio = frameSize.width/frameSize.height;
-        if(ratio < 2){
-            cc.view.setDesignResolutionSize(DESIGN_RESOLUTION_WIDTH,DESIGN_RESOLUTION_HEIGHT, cc.ResolutionPolicy.FIXED_HEIGHT);
-        }else{
-            cc.view.setDesignResolutionSize(DESIGN_RESOLUTION_WIDTH,DESIGN_RESOLUTION_WIDTH/2, cc.ResolutionPolicy.SHOW_ALL);
+    cc.view.setDesignResolutionSize(480,720,cc.ResolutionPolicy.SHOW_ALL);
+    cc.view.resizeWithBrowserSize(true);
+    cc.director.setProjection(cc.Director.PROJECTION_2D);
+    if (cc.sys.isNative) {
+        var searchPaths = jsb.fileUtils.getSearchPaths();
+        searchPaths.push('script');
+        if (cc.sys.os == cc.sys.OS_IOS || cc.sys.os == cc.sys.OS_OSX) {
+            searchPaths.push("res");
+            searchPaths.push("src");
         }
-
-        // The game will be resized when browser size change
-        cc.view.resizeWithBrowserSize(true);
-        //socket
-        gv.gameClient = new GameClient();
-        gv.poolObjects = new PoolObject();
-        //modules
-        testnetwork.connector = new testnetwork.Connector(gv.gameClient);
-
-        fr.view(ScreenMenu);
+        jsb.fileUtils.setSearchPaths(searchPaths);
+    }
+    //load resources
+    cc.LoaderScene.preload(g_mainmenu, function () {
+        cc.director.runScene(SysMenu.scene());
     }, this);
 };
+
 cc.game.run();
---
+++ src/SysMenu.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Admin on 6/1/2020.
+ */
+var SysMenu = cc.Layer.extend({
+
+});
